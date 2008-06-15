dcca6c65aa25f5aa330dba7b0be20d50941fefa3
Spectre <Spectre@a6a5f009-272a-4b40-a74d-5f9816a51f88>
2008-06-15 14:21:53 +0000

    Left out a few renames, now shows AspireCore on info in console / on ServerStatusPlugin
    git-svn-id: svn://svn.aspiredev.org/AspireCore/trunk@57 a6a5f009-272a-4b40-a74d-5f9816a51f88

--- src/scripts/src/ServerStatusPlugin/ServerStatusPlugin.cpp
+++ src/scripts/src/ServerStatusPlugin/ServerStatusPlugin.cpp
@@ -314,9 +314,9 @@ void StatDumper::DumpStats()
     {
         // Dump server information.
 #ifdef WIN32
-		fprintf(f, "    <platform>Ascent %s r%u/%s-Win-%s (www.ascentemu.com)</platform>\n", BUILD_TAG, BUILD_REVISION, CONFIG, ARCH);		
+		fprintf(f, "    <platform>AspireCore %s r%u/%s-Win-%s (www.aspiredev.org)</platform>\n", BUILD_TAG, BUILD_REVISION, CONFIG, ARCH);		
 #else
-		fprintf(f, "    <platform>Ascent %s r%u/%s-%s (www.ascentemu.com)</platform>\n", BUILD_TAG, BUILD_REVISION, PLATFORM_TEXT, ARCH);
+		fprintf(f, "    <platform>AspireCore %s r%u/%s-%s (www.aspiredev.org)</platform>\n", BUILD_TAG, BUILD_REVISION, PLATFORM_TEXT, ARCH);
 #endif
 
         char uptime[80];
