1f00d82ea914f36952ed78ac5154fc1525fb40a6
Chameleon <Chameleon@a6a5f009-272a-4b40-a74d-5f9816a51f88>
2008-08-27 16:58:35 +0000

    createarenateam command can handle team names with more than one word
    git-svn-id: svn://svn.aspiredev.org/AspireCore/trunk@274 a6a5f009-272a-4b40-a74d-5f9816a51f88

--- src/ascent-world/Level3.cpp
+++ src/ascent-world/Level3.cpp
@@ -2678,11 +2678,15 @@ bool ChatHandler::HandleCreateArenaTeamCommands(const char * args, WorldSession 
 	char name[1000];
 	uint32 real_type;
 	Player * plr = getSelectedChar(m_session, true);
-	if(sscanf(args, "%u %s", &arena_team_type, name) != 2)
+	size_t argslen = strlen(args);
+	if( argslen < 3 || argslen > 999)
 	{
-		SystemMessage(m_session, "Invalid syntax.");
+		RedSystemMessage(m_session, "Invalid syntax.");
 		return true;
 	}
+	char team_type[2] = {args[0], 0};
+	arena_team_type = atoi(team_type);
+	strncpy(name, &args[2],998);
 
 	switch(arena_team_type)
 	{
