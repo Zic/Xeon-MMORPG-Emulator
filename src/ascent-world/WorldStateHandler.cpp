eae73e454ec0084ec60f60bb88892f40a2287217
Spectre <Spectre@a6a5f009-272a-4b40-a74d-5f9816a51f88>
2008-11-14 20:22:21 +0000

    Committing the two files I've been working on for WorldStates, they are not yet implemented nor complete, and have not been tested in anyway other then compiling.
    git-svn-id: svn://svn.aspiredev.org/AspireCore/trunk@437 a6a5f009-272a-4b40-a74d-5f9816a51f88

---
+++ src/ascent-world/WorldStateHandler.cpp
@@ -0,0 +1,117 @@
+/*
+ *					AspireCore
+ * Copyright (C) 2008 AspireDev <http://www.aspiredev.org/>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "StdAfx.h"
+#include "WorldStateHandler.h"
+
+WorldStateHandler::WorldStateHandler(MapMgr *mgr)
+{
+	m_Map = mgr;
+}
+
+WorldStateHandler::~WorldStateHandler()
+{
+	map<uint32, WorldStateData*>::iterator Itr;
+	for(Itr = worldStates.begin(); Itr != worldStates.end(); Itr++)
+		delete Itr->second;
+		
+}
+
+WorldStateData *WorldStateHandler::InitializeWorldState(uint32 Index, uint32 Val, int32 ZoneRef, int32 FactionRef)
+{
+	map<uint32, WorldStateData*>::iterator Itr = worldStates.find(Index);
+
+	if(Itr == worldStates.end())
+	{
+		WorldStateData *Data = new WorldStateData;
+		Data->Value = Val;
+		Data->Faction = FactionRef;
+		Data->Zone = ZoneRef;
+		worldStates.insert(make_pair(Index, Data));
+		return Data;
+	}
+	else
+	{
+		Itr->second->Value = Val;
+		Itr->second->Faction = FactionRef;
+		Itr->second->Zone = ZoneRef;
+		return Itr->second;
+	}
+
+	return NULL;
+}
+
+void WorldStateHandler::UpdateWorldState(uint32 Index, uint32 Val)
+{
+	WorldStateData *wState;
+	map<uint32, WorldStateData*>::iterator Itr = worldStates.find(Index);
+	if(Itr == worldStates.end()) // Updating a non-existant wordstate? hmm..ok
+		wState = InitializeWorldState(Index, Val);
+	else
+	{
+		wState= Itr->second;
+		wState->Value = Val;
+	}
+
+	WorldPacket data(SMSG_UPDATE_WORLD_STATE, 10); // most likely not correct
+	data << Index << Val;
+	SendWorldStatePacket(wState, &data);
+}
+
+void WorldStateHandler::SendWorldStatePacket(WorldStateData *wState, WorldPacket *Data)
+{
+	PlayerStorageMap::iterator Itr;
+	for(Itr = m_Map->m_PlayerStorage.begin(); Itr != m_Map->m_PlayerStorage.end(); Itr++)
+	{
+		Player *Plr = Itr->second;
+		uint8 Team = Plr->GetTeam() == 0 ? 0 : 1;
+		
+		if(Plr->GetSession() == NULL)
+			continue;
+		if(wState->Faction != FactionRef_ALL && wState->Faction != Team)
+			continue;
+		if(wState->Zone != ZoneRef_ALL && wState->Zone != Plr->GetAreaID())
+			continue;
+
+		Plr->GetSession()->SendPacket(Data);
+	}
+}
+
+void WorldStateHandler::SendWorldStates(Player *Plr)
+{
+	WorldPacket data(SMSG_INIT_WORLD_STATES, (worldStates.size() * 8) + 32);
+	map<uint32, WorldStateData*>::iterator Itr;
+	uint8 Team = Plr->GetTeam() == 0 ? 0 : 1;
+
+	data << m_Map->GetMapId();
+	data << Plr->GetZoneId();
+	data << Plr->GetAreaID();
+
+	for(Itr = worldStates.begin(); Itr != worldStates.end(); Itr++)
+	{
+		if(Plr->GetSession() == NULL)
+			continue;
+		if(Itr->second->Faction != FactionRef_ALL && Itr->second->Faction != Team)
+			continue;
+		if(Itr->second->Zone != ZoneRef_ALL && Itr->second->Zone != Plr->GetAreaID())
+			continue;
+		data << Itr->first << Itr->second->Value;
+	}
+	Plr->GetSession()->SendPacket(&data);	
+}
---
+++ src/ascent-world/WorldStateHandler.h
@@ -0,0 +1,57 @@
+/*
+ *					AspireCore
+ * Copyright (C) 2008 AspireDev <http://www.aspiredev.org/>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+// Some credit goes to summit and it's devs, as I've seen their implimentation
+// And based some things off of the data they had
+
+#ifndef WorldStateHandler_H
+#define WorldStateHandler_H
+
+#define FactionRef_ALL -1
+#define ZoneRef_ALL -1
+
+class MapMgr;
+
+struct WorldStateData
+{
+	uint32 Value;
+	int32 Zone; // Int32 to allow negative values
+	int32 Faction; // same here
+};
+
+class WorldStateHandler
+{
+	public:
+		WorldStateHandler(MapMgr *mgr);
+		~WorldStateHandler();
+
+		// WorldState Handling
+		WorldStateData *InitializeWorldState(uint32 Index, uint32 Val, int32 ZoneRef = ZoneRef_ALL, int32 FactionRef = FactionRef_ALL);
+		void UpdateWorldState(uint32 Index, uint32 Val);
+		void SendWorldStates(Player *Plr);
+
+	private:
+		MapMgr *m_Map;
+		std::map<uint32, WorldStateData*> worldStates;
+
+	protected:
+		void SendWorldStatePacket(WorldStateData *wState, WorldPacket *Data);
+};
+
+#endif // WorldStateHandler_H
