7f3c42e3a88bac1b044718f76f17ebf67f310354
Aceindy <Aceindy@a6a5f009-272a-4b40-a74d-5f9816a51f88>
2009-01-09 23:50:14 +0000

    First commit feels good to be back in business. * First let's attend my baby; DEBUG_LOG doesn't support multiple strings (I prefer Log.Debug anyway)
    git-svn-id: svn://svn.aspiredev.org/AspireCore/branches/Hearthstone@755 a6a5f009-272a-4b40-a74d-5f9816a51f88

--- src/hearthstone-world/WeatherMgr.cpp
+++ src/hearthstone-world/WeatherMgr.cpp
@@ -199,7 +199,7 @@ void WeatherInfo::_GenerateWeather()
 	SendUpdate();
 
 	sEventMgr.AddEvent(CAST(WeatherInfo,shared_from_this()), &WeatherInfo::BuildUp, EVENT_WEATHER_UPDATE, (uint32)(m_totalTime/ceil(m_maxDensity/WEATHER_DENSITY_UPDATE)*2), 0,0);
-	DEBUG_LOG("WeatherMgr", "Forecast for zone:%d new type:%d new interval:%d ms",m_zoneId,m_currentEffect,(uint32)(m_totalTime/ceil(m_maxDensity/WEATHER_DENSITY_UPDATE)*2));
+	Log.Debug("WeatherMgr", "Forecast for zone:%d new type:%d new interval:%d ms",m_zoneId,m_currentEffect,(uint32)(m_totalTime/ceil(m_maxDensity/WEATHER_DENSITY_UPDATE)*2));
 }
 
 void WeatherInfo::BuildUp()
