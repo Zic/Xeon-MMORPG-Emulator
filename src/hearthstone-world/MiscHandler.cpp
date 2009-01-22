1be7a7d97df6229217ff22c1231a4f2e4f684fa4
pepsi1x1 <pepsi1x1@a6a5f009-272a-4b40-a74d-5f9816a51f88>
2009-01-22 15:03:37 +0000

    *Corrections to CMSG_SET_ACTION_BUTTON handler
    git-svn-id: svn://svn.aspiredev.org/AspireCore/branches/Hearthstone@911 a6a5f009-272a-4b40-a74d-5f9816a51f88

--- src/hearthstone-world/MiscHandler.cpp
+++ src/hearthstone-world/MiscHandler.cpp
@@ -1032,12 +1032,12 @@ void WorldSession::HandleSetActionButtonOpcode(WorldPacket& recv_data)
 		if(type == 64 || type == 65) 
 		{
 			DEBUG_LOG( "MISC: Added Macro %u into button %u", action, button );
-			GetPlayer()->setAction(button,action,misc,type);
+			GetPlayer()->setAction(button,action,type,misc);
 		}
 		else if(type == 128)
 		{
 			DEBUG_LOG( "MISC: Added Item %u into button %u", action, button );
-			GetPlayer()->setAction(button,action,misc,type);
+			GetPlayer()->setAction(button,action,type,misc);
 		}
 		else if(type == 0)
 		{
