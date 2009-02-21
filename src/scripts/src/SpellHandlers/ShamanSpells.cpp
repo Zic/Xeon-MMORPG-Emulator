0878c60138a41179646b72d41edb719c3744bba3
Spidey <Spidey@a6a5f009-272a-4b40-a74d-5f9816a51f88>
2009-02-21 13:23:04 +0000

    Changed the debug line on ShamanSpells.cpp to source from ShamanSpells instead of ShamanSpells.cpp
    git-svn-id: svn://svn.aspiredev.org/AspireCore/trunk@1083 a6a5f009-272a-4b40-a74d-5f9816a51f88

--- src/scripts/src/SpellHandlers/ShamanSpells.cpp
+++ src/scripts/src/SpellHandlers/ShamanSpells.cpp
@@ -80,7 +80,7 @@ bool RockbiterWeapon(uint32 i, Spell* pSpell)
         if(Slot < 0) return true;
     }
 
-    sLog.outDebug("ShamanSpells.cpp :: Rockbiter Weapon Rank %u, enchant %u, slot %u", pSpell->m_spellInfo->RankNumber,
+    sLog.outDebug("ShamanSpells :: Rockbiter Weapon Rank %u, enchant %u, slot %u", pSpell->m_spellInfo->RankNumber,
         enchantment_entry, Slot);
     
     return true;
