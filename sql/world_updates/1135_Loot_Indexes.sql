9ac687bdcb673ef9bc9f6ba6c0b88e0d25369311
Aceindy <Aceindy@a6a5f009-272a-4b40-a74d-5f9816a51f88>
2009-03-08 00:07:10 +0000

    * Sorry, other way around (might throw error otherwise)
    git-svn-id: svn://svn.aspiredev.org/AspireCore/branches/Hearthstone@1136 a6a5f009-272a-4b40-a74d-5f9816a51f88

--- sql/world_updates/1135_Loot_Indexes.sql
+++ sql/world_updates/1135_Loot_Indexes.sql
@@ -1,20 +1,20 @@
 -- Usable after HS >R1134
 
-ALTER TABLE `creatureloot` DROP COLUMN `index`;
 ALTER TABLE `creatureloot` DROP KEY `PRIMARY`, ADD PRIMARY KEY (`entryid`,`itemid`);
-ALTER TABLE `disenchantingloot` DROP COLUMN `index`;
+ALTER TABLE `creatureloot` DROP COLUMN `index`;
 ALTER TABLE `disenchantingloot` DROP KEY `PRIMARY`, ADD PRIMARY KEY (`entryid`,`itemid`);
-ALTER TABLE `fishingloot` DROP COLUMN `index`;
+ALTER TABLE `disenchantingloot` DROP COLUMN `index`;
 ALTER TABLE `fishingloot` DROP KEY `PRIMARY`, ADD PRIMARY KEY (`entryid`,`itemid`);
-ALTER TABLE `itemloot` DROP COLUMN `index`;
+ALTER TABLE `fishingloot` DROP COLUMN `index`;
 ALTER TABLE `itemloot` DROP KEY `PRIMARY`, ADD PRIMARY KEY (`entryid`,`itemid`);
-ALTER TABLE `millingloot` DROP COLUMN `index`;
+ALTER TABLE `itemloot` DROP COLUMN `index`;
 ALTER TABLE `millingloot` DROP KEY `PRIMARY`, ADD PRIMARY KEY (`entryid`,`itemid`);
-ALTER TABLE `objectloot` DROP COLUMN `index`;
+ALTER TABLE `millingloot` DROP COLUMN `index`;
 ALTER TABLE `objectloot` DROP KEY `PRIMARY`, ADD PRIMARY KEY (`entryid`,`itemid`);
-ALTER TABLE `pickpocketingloot` DROP COLUMN `index`;
+ALTER TABLE `objectloot` DROP COLUMN `index`;
 ALTER TABLE `pickpocketingloot` DROP KEY `PRIMARY`, ADD PRIMARY KEY (`entryid`,`itemid`);
-ALTER TABLE `prospectingloot` DROP COLUMN `index`;
+ALTER TABLE `pickpocketingloot` DROP COLUMN `index`;
 ALTER TABLE `prospectingloot` DROP KEY `PRIMARY`, ADD PRIMARY KEY (`entryid`,`itemid`);
-ALTER TABLE `skinningloot` DROP COLUMN `index`;
+ALTER TABLE `prospectingloot` DROP COLUMN `index`;
 ALTER TABLE `skinningloot` DROP KEY `PRIMARY`, ADD PRIMARY KEY (`entryid`,`itemid`);
+ALTER TABLE `skinningloot` DROP COLUMN `index`;
